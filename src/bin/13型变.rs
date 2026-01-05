cb972b515c06507715e7ca46ae1db8dc740dc999
sunzhen <sunzhen@jvsit.com>
2026-01-05 22:01:14 +0800

    xx

--- src/bin/13型变.rs
+++ src/bin/13型变.rs
@@ -4,6 +4,7 @@ struct Foo<'r> {
     _phantom: PhantomData<&'r ()>, //针对不可变引用 &r  是斜变
 }
 
+// 测试协变的情况
 fn foo<'short, 'long: 'short>(mut short_foo: Foo<'short>, mut long_foo: Foo<'long>) {
     short_foo = long_foo; //  因为 lang是 short 子类 所以 lang 可以赋值给子类 long 的 含义是子类 
     // long_foo = short_foo; // 报错
